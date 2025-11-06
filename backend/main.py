0b9680e275fd538a330cf247bdaf1b0085e548e3
jiangzhenjie <12102716@qq.com>
2025-11-06 17:18:46 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,18 @@
+from pathlib import Path
+
+import uvicorn
+
+from backend.core.registrar import register_app
+
+app = register_app()
 
 
+if __name__ == '__main__':
+    try:
+        config = uvicorn.Config(app=f"{Path(__file__).stem}:app", reload=True)
+        server = uvicorn.Server(config)
+        server.run()
+    except Exception as e:
+        raise e
 
-# app = register_app()
 
