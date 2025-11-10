e352db529fdbd9fd45a9c21d1afd63d026457a02
jiangzhenjie <12102716@qq.com>
2025-11-10 16:08:34 +0800

    [fix]: id_key自动初始化

--- backend/app/env/model/env_instance.py
+++ backend/app/env/model/env_instance.py
@@ -9,7 +9,7 @@ class EnvInstance(Base):
 
     __tablename__ = "env_instance"
 
-    id: Mapped[id_key]
+    id: Mapped[id_key] = mapped_column(init=False)
     name: Mapped[str] = mapped_column(sa.String(64), unique=True, comment="环境配置实例名称")
     template_id: Mapped[int] = mapped_column(comment="环境配置模版 ID")
     params: Mapped[dict] = mapped_column(sa.JSON, comment="环境配置实例参数")
--- backend/app/env/model/env_template.py
+++ backend/app/env/model/env_template.py
@@ -1,7 +1,9 @@
 import sqlalchemy as sa
 from sqlalchemy.orm import Mapped, mapped_column
 
-from backend.common.model import Base, id_key
+from backend.common.model import Base
+
+from backend.common.model import id_key
 
 
 class EnvTemplate(Base):
@@ -9,6 +11,6 @@ class EnvTemplate(Base):
 
     __tablename__ = "env_template"
 
-    id: Mapped[id_key]
+    id: Mapped[id_key] = mapped_column(init=False)
     name: Mapped[str] = mapped_column(sa.String(64), unique=True, comment="环境配置模版名称")
     param_schema: Mapped[dict] = mapped_column(sa.JSON, comment="环境配置模版参数 schema")
