a3a2d852cb6416bde285e049da19cfaff8a44fd2
jiangzhenjie <12102716@qq.com>
2025-11-10 16:42:02 +0800

    [upd]: 统一schema格式

--- backend/app/env/schema/env_instance.py
+++ backend/app/env/schema/env_instance.py
@@ -1,11 +1,31 @@
-from pydantic import Field
+from datetime import datetime
+
+from pydantic import Field, ConfigDict
+
 
 from backend.common.schema import SchemaBase
 
 
-class EnvInstanceParam(SchemaBase):
+class EnvInstanceParamBase(SchemaBase):
     """环境配置实例参数"""
 
     template_id: int = Field(description="环境配置模版 ID")
     name: str = Field(description="环境配置实例名称")
     params: dict = Field(description="环境配置实例参数")
+
+
+class CreateEnvInstanceParam(EnvInstanceParamBase):
+    """创建环境配置实例参数"""
+
+
+class GetEnvInstanceDetail(EnvInstanceParamBase):
+    """环境配置实例详情"""
+
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int = Field(description="实例ID")
+    template_id: int = Field(description="环境配置模版 ID")
+    name: str = Field(description="环境配置实例名称")
+    params: dict = Field(description="环境配置实例参数")
+    create_at: datetime = Field(description="创建时间")
+    update_at: datetime | None = Field(None, description="更新时间")
--- backend/app/env/schema/env_template.py
+++ backend/app/env/schema/env_template.py
@@ -1,10 +1,28 @@
-from pydantic import Field
+from datetime import datetime
+
+from pydantic import ConfigDict, Field
 
 from backend.common.schema import SchemaBase
 
 
-class EnvTemplateParam(SchemaBase):
+class EnvTemplateParamBase(SchemaBase):
     """环境配置模版参数"""
 
     name: str = Field(description="环境配置模版名称")
     param_schema: dict = Field(description="环境配置模版参数 schema")
+
+
+class CreateEnvTemplateParam(EnvTemplateParamBase):
+    """创建环境配置模版参数"""
+
+
+class GetEnvTemplateDetail(EnvTemplateParamBase):
+    """环境配置模版详情"""
+
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int = Field(description="模版ID")
+    name: str = Field(description="环境配置模版名称")
+    param_schema: dict = Field(description="环境配置模版参数 schema")
+    create_at: datetime = Field(description="创建时间")
+    update_at: datetime = Field(None, description="更新时间")
