b71562578cee5036f281cd94f2645b2108de34a6
jiangzhenjie <12102716@qq.com>
2025-11-10 14:14:14 +0800

    Update router.py

--- backend/app/router.py
+++ backend/app/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from backend.app.agent.api.router import v1 as agent_v1
+from backend.app.env.api.router import v1 as env_v1
 
 route = APIRouter()
 route.include_router(agent_v1)
+route.include_router(env_v1)
