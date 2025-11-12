20901ad11db0ea0808d207c1b86b57758cc48e89
jiangzhenjie <12102716@qq.com>
2025-11-12 16:25:39 +0800

    [fix]: 更新路由

--- backend/app/agent/api/router.py
+++ backend/app/agent/api/router.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-from backend.app.agent.api.v1.agent_meta import router as upload_router
+from backend.app.agent.api.v1.agent_meta import router as agent_meta_router
 from backend.core.conf import settings
 
-v1 = APIRouter(prefix=settings.FAST_API_V1_PATH)
+v1 = APIRouter(prefix=f"{settings.FAST_API_V1_PATH}/agent_meta", tags=["智能体元数据"])
 
-v1.include_router(upload_router)
+v1.include_router(agent_meta_router)
